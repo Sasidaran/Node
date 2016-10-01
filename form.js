073524aa364c4f6c8021b9336cda0b6c82c01685
Sasidaran S <sasidaran.s@tcs.com>
2016-10-01 15:08:00 +0200

    Growl desktop notifications

--- form.js
+++ form.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var server = http.createServer(function (req, res) {
     displayForm(res);
 });
+var growl = require('growl')
 
 function displayForm(res) {
     fs.readFile('form.html', function (err, data) {
@@ -12,6 +13,17 @@ function displayForm(res) {
         });
         res.write(data);
         res.end();
+        //growl('You have mail!')
+				growl('5 new messages', { sticky: true })
+				//growl('5 new emails', { title: 'Email Client', image: 'Safari', sticky: true })
+				//growl('Message with title', { title: 'Title'})
+				//growl('Set priority', { priority: 2 })
+				//growl('Show Safari icon', { image: 'Safari' })
+				//growl('Show icon', { image: 'path/to/icon.icns' })
+				//growl('Show image', { image: 'path/to/my.image.png' })
+				//growl('Show png filesystem icon', { image: 'png' })
+				//growl('Show pdf filesystem icon', { image: 'article.pdf' })
+
     });
 }
 
